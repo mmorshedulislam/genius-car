76d3a394cdf33c20464013bc345bc38709b67a98
MORSHED-HOME <morshed952640@gmail.com>
2022-10-30 22:00:48 +0600

    Genius car client set up

---
+++ genius-car-client/src/Router/Routes/Routes.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([
+    {
+        path: '/', 
+        element: <div>Hello World!</div>
+    }
+])
+
+export default routes; 
