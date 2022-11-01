25315d7de7f4f7381bdb9a207c5677ae789e0e43
MORSHED-OFFICE <morshed952640@gmail.com>
2022-11-01 12:43:20 +0600

    Team section some added

---
+++ genius-car-client/src/Pages/Home/Team/Team.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Team = () => {
+    /* 
+    <img src="https://i.ibb.co/L5z2RNW/1.jpg" alt="1" border="0">
+    <img src="https://i.ibb.co/NVvcL2Z/2.jpg" alt="2" border="0">
+    <img src="https://i.ibb.co/vJ4kT09/3.jpg" alt="3" border="0">
+    */
+  return (
+    <div>
+      <div className="text-center my-14  w-1/2 mx-auto">
+        <h2 className="text-xl text-orange-600">Services</h2>
+        <h2 className="text-5xl font-bold my-3">Our Service Area</h2>
+        <p>
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
